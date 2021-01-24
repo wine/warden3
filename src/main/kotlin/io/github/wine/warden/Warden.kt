4ab4218d9355088a311cc8867c46ffec84ddb2ed
Ossian Winter <ossian-winter@riseup.net>
2021-01-24 19:32:34 +0100

    Add documentation to the Warden singleton

--- src/main/kotlin/io/github/wine/warden/Warden.kt
+++ src/main/kotlin/io/github/wine/warden/Warden.kt
@@ -4,6 +4,11 @@ import io.github.wine.warden.event.bus.EventBus
 import io.github.wine.warden.patch.impl.patches.MinecraftPatch
 import io.github.wine.warden.patch.registry.PatchRegistry
 
+/**
+ * Warden singleton, holds necessary fields such as the [EventBus] and [PatchRegistry],
+ * a Kotlin object will always be the same instance no matter how many times
+ * we request it, there can only be one instance of this class.
+ */
 object Warden {
 
     val patches = PatchRegistry()
