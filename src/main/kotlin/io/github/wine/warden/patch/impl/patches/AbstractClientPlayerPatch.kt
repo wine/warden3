e4f7ab707134bd59a4aef51978e739d60766fb0d
Ossian Winter <ossian-winter@riseup.net>
2021-01-25 14:31:22 +0100

    Add skin fix

---
+++ src/main/kotlin/io/github/wine/warden/patch/impl/patches/AbstractClientPlayerPatch.kt
@@ -0,0 +1,44 @@
+package io.github.wine.warden.patch.impl.patches
+
+import io.github.wine.warden.patch.annotate.Inject
+import io.github.wine.warden.patch.impl.ClassPatch
+import org.objectweb.asm.Opcodes.*
+import org.objectweb.asm.Type
+import org.objectweb.asm.tree.*
+import java.util.concurrent.atomic.AtomicReference
+
+class AbstractClientPlayerPatch : ClassPatch("net/minecraft/src/AbstractClientPlayer") {
+
+    @Inject("getSkinUrl", "(Ljava/lang/String;)Ljava/lang/String;")
+    fun patchGetSkinUrl(methodNode: MethodNode) {
+        val instructions = InsnList()
+
+        // Load name reference onto the stack
+        instructions.add(VarInsnNode(ALOAD, 0))
+
+        instructions.add(
+            MethodInsnNode(
+                INVOKESTATIC,
+                Type.getInternalName(this.javaClass),
+                "getSkinUrl",
+                "(Ljava/lang/String;)Ljava/lang/String;",
+                false
+            )
+        )
+
+        // Return the String from getSkinUrl
+        instructions.add(InsnNode(ARETURN))
+
+        methodNode.instructions.insert(instructions)
+    }
+
+    companion object {
+        private const val API_URL = "https://minotar.net"
+
+        @JvmStatic
+        fun getSkinUrl(name: String): String {
+            println(name)
+            return "$API_URL/skin/$name"
+        }
+    }
+}
