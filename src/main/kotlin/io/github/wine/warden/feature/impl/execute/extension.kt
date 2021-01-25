1f575adc9e6d306ea1c7e76c1400bebda05e06d0
Ossian Winter <ossian-winter@riseup.net>
2021-01-25 12:10:05 +0100

    Fix unclear floating magic number

--- src/main/kotlin/io/github/wine/warden/feature/impl/execute/extension.kt
+++ src/main/kotlin/io/github/wine/warden/feature/impl/execute/extension.kt
@@ -4,12 +4,14 @@ import com.xenomachina.argparser.ShowHelpException
 import net.minecraft.src.Minecraft
 import java.io.StringWriter
 
+private const val HELP_COLUMNS = 60
+
 fun Any.printToMinecraft() {
     Minecraft.getMinecraft().ingameGUI.chatGUI.printChatMessage(this.toString())
 }
 
 fun ShowHelpException.printToMinecraft(identifier: String) {
-    val help = StringWriter().apply { printUserMessage(this, identifier, 60) }.toString().split("\n")
+    val help = StringWriter().apply { printUserMessage(this, identifier, HELP_COLUMNS) }.toString().split("\n")
     help.forEach(Minecraft.getMinecraft().ingameGUI.chatGUI::printChatMessage)
 }
 
