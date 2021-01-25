514cb679998ceb37968d291a954ffa0030b9da39
Ossian Winter <ossian-winter@riseup.net>
2021-01-25 08:21:44 +0100

    Create sprint feature

---
+++ src/main/kotlin/io/github/wine/warden/feature/impl/intermittent/features/SprintFeature.kt
@@ -0,0 +1,33 @@
+package io.github.wine.warden.feature.impl.intermittent.features
+
+import io.github.wine.warden.Warden
+import io.github.wine.warden.event.bus.EventSubscriber
+import io.github.wine.warden.event.impl.game.events.TickEvent
+import io.github.wine.warden.feature.impl.intermittent.IntermittentFeature
+import net.minecraft.src.EntityClientPlayerMP
+
+class SprintFeature : IntermittentFeature("sprint") {
+
+    /**
+     * todo: replace [TickEvent] with an event published from the players movement logic method.
+     */
+    private val tickSubscriber = EventSubscriber<TickEvent> {
+        it.game.thePlayer.isSprinting = it.game.thePlayer.canNormallySprint()
+    }
+
+    override fun onEnable() {
+        Warden.bus.subscribe(tickSubscriber)
+    }
+
+    override fun onDisable() {
+        Warden.bus.unsubscribe(tickSubscriber)
+    }
+
+    /**
+     * Whether or not the [EntityClientPlayerMP] can sprint by just
+     * holding the sprint modifier key.
+     */
+    private fun EntityClientPlayerMP.canNormallySprint(): Boolean {
+        return (this.moveForward != 0f || this.moveStrafing != 0f) && !this.isCollidedHorizontally
+    }
+}
