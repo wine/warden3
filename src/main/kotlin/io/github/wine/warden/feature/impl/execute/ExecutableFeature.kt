ea6b9ff2973806aa2fd26a28ab4bfb1ccb24e972
Ossian Winter <ossian-winter@riseup.net>
2021-01-25 11:39:36 +0100

    Catch exceptions when executing features

--- src/main/kotlin/io/github/wine/warden/feature/impl/execute/ExecutableFeature.kt
+++ src/main/kotlin/io/github/wine/warden/feature/impl/execute/ExecutableFeature.kt
@@ -1,15 +1,26 @@
 package io.github.wine.warden.feature.impl.execute
 
+import com.xenomachina.argparser.ShowHelpException
 import io.github.wine.warden.Warden
 import io.github.wine.warden.event.bus.EventSubscriber
 import io.github.wine.warden.event.impl.client.ExecuteCommandEvent
 import io.github.wine.warden.feature.Feature
+import net.minecraft.src.Minecraft
+import java.io.StringWriter
 
 abstract class ExecutableFeature(override val identifier: String) : Feature {
 
-    private val executeCommandSubscriber = EventSubscriber<ExecuteCommandEvent> {
-        if (it.identifier == identifier)
-            execute(it.arguments)
+    private val executeCommandSubscriber = EventSubscriber<ExecuteCommandEvent> { event ->
+        if (event.identifier == identifier) {
+            try {
+                execute(event.arguments)
+            } catch(e: ShowHelpException) {
+                val help = StringWriter().apply { e.printUserMessage(this, identifier, 60) }.toString().split("\n")
+                help.forEach(Minecraft.getMinecraft().ingameGUI.chatGUI::printChatMessage)
+            } catch (e: Exception) {
+                Minecraft.getMinecraft().ingameGUI.chatGUI.printChatMessage(e.localizedMessage)
+            }
+        }
     }
 
     init {
