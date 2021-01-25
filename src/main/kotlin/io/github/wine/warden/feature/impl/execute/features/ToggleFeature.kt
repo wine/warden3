bcd695197306715532134e2a6e62765578afadcb
Ossian Winter <ossian-winter@riseup.net>
2021-01-25 12:01:11 +0100

    Use new transformer and output to chat

--- src/main/kotlin/io/github/wine/warden/feature/impl/execute/features/ToggleFeature.kt
+++ src/main/kotlin/io/github/wine/warden/feature/impl/execute/features/ToggleFeature.kt
@@ -1,21 +1,26 @@
 package io.github.wine.warden.feature.impl.execute.features
 
 import com.xenomachina.argparser.ArgParser
-import io.github.wine.warden.Warden
 import io.github.wine.warden.feature.impl.execute.ExecutableFeature
-import io.github.wine.warden.feature.impl.intermittent.IntermittentFeature
+import io.github.wine.warden.feature.impl.execute.printToMinecraft
+import io.github.wine.warden.feature.impl.execute.transform.toIntermittentFeature
 
 class ToggleFeature : ExecutableFeature("toggle") {
 
     override fun execute(arguments: Array<String>) {
         ArgParser(arguments).parseInto(::ToggleArguments).run {
-            feature?.toggle()
+            feature.toggle()
+
+            var output = "Toggled ${feature.identifier} "
+            output += if (feature.state) "§a" + "on" else "§c" + "off"
+
+            output.printToMinecraft()
         }
     }
 
     private class ToggleArguments(parser: ArgParser) {
-        val feature by parser.positional("TARGET", "Identifier of the target intermittent feature") {
-            Warden.features[this] as? IntermittentFeature
+        val feature by parser.positional("TARGET", "identifier of the target intermittent feature") {
+            toIntermittentFeature()
         }
     }
 }
