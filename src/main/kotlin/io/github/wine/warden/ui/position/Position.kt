c92c3e46fece9a323eee8e6bd31c8f93e3a95172
Ossian Winter <ossian-winter@riseup.net>
2021-01-26 16:35:55 +0100

    Add position type for elements

---
+++ src/main/kotlin/io/github/wine/warden/ui/position/Position.kt
@@ -0,0 +1,3 @@
+package io.github.wine.warden.ui.position
+
+open class Position(var x: Int, var y: Int)
---
+++ src/main/kotlin/io/github/wine/warden/ui/position/impl/Space.kt
@@ -0,0 +1,5 @@
+package io.github.wine.warden.ui.position.impl
+
+import io.github.wine.warden.ui.position.Position
+
+class Space(x: Int, y: Int, var width: Int, var height: Int) : Position(x, y)
