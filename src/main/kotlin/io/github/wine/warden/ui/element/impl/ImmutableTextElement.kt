ff2692e0c114806830d0188000733bc15cd95332
Ossian Winter <ossian-winter@riseup.net>
2021-01-26 16:36:04 +0100

    Create immutable text element

---
+++ src/main/kotlin/io/github/wine/warden/ui/element/impl/ImmutableTextElement.kt
@@ -0,0 +1,14 @@
+package io.github.wine.warden.ui.element.impl
+
+import io.github.wine.warden.ui.element.Element
+import io.github.wine.warden.ui.position.Position
+import net.minecraft.src.FontRenderer
+import java.awt.Color
+
+class ImmutableTextElement(override val position: Position, private val text: String, private val color: Int = Color.WHITE.rgb) :
+    Element {
+
+    override fun render(fontRenderer: FontRenderer) {
+        fontRenderer.drawStringWithShadow(text, position.x, position.y, color)
+    }
+}
