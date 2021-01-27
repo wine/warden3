48c276d4950bd631649e8269f0821f48f6bfebbc
Ossian Winter <ossian-winter@riseup.net>
2021-01-27 12:24:59 +0100

    Text elements color defaults to the current color schemes

--- src/main/kotlin/io/github/wine/warden/ui/element/impl/ImmutableTextElement.kt
+++ src/main/kotlin/io/github/wine/warden/ui/element/impl/ImmutableTextElement.kt
@@ -1,11 +1,12 @@
 package io.github.wine.warden.ui.element.impl
 
+import io.github.wine.warden.ui.color.scheme.ColorScheme
 import io.github.wine.warden.ui.element.Element
 import io.github.wine.warden.ui.position.Position
 import net.minecraft.src.FontRenderer
 import java.awt.Color
 
-open class ImmutableTextElement(override val position: Position, private val text: String, private val color: Int = Color.WHITE.rgb) :
+open class ImmutableTextElement(override val position: Position, private val text: String, private val color: Int = ColorScheme.text.rgb) :
     Element {
 
     override fun render(fontRenderer: FontRenderer) {
