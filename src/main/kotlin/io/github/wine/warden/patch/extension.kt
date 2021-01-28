117289d61c588d13ba76f4b42269e9e9c053997f
Ossian Winter <ossian-winter@riseup.net>
2021-01-28 09:27:06 +0100

    Add MethodNode extension function for finding method instructions

--- src/main/kotlin/io/github/wine/warden/patch/extension.kt
+++ src/main/kotlin/io/github/wine/warden/patch/extension.kt
@@ -3,6 +3,7 @@ package io.github.wine.warden.patch
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.MethodNode
 import org.objectweb.asm.tree.LdcInsnNode
+import org.objectweb.asm.tree.MethodInsnNode
 
 
 fun MethodNode.insertInstructionBeforeReturn(instructionNode: AbstractInsnNode) {
@@ -14,4 +15,13 @@ fun MethodNode.findLdcInstruction(string: String): LdcInsnNode? {
     return this.instructions.toArray()
         .filterIsInstance<LdcInsnNode>()
         .firstOrNull { it.cst.toString() == string }
+}
+
+fun MethodNode.findMethodInstruction(opcode: Int, owner: String, name: String, descriptor: String): MethodInsnNode? {
+    return this.instructions.toArray()
+        .filterIsInstance<MethodInsnNode>()
+        .filter { it.opcode == opcode }
+        .filter { it.owner == owner }
+        .filter { it.name == name }
+        .find { it.desc == descriptor }
 }
