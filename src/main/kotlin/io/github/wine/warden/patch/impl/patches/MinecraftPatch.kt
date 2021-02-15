8d9e6c0f03c1135d63f2da59661b9b39aa6d2cc2
Ossian Winter <ossian-winter@riseup.net>
2021-02-15 22:02:42 +0100

    Create start game event

--- src/main/kotlin/io/github/wine/warden/patch/impl/patches/MinecraftPatch.kt
+++ src/main/kotlin/io/github/wine/warden/patch/impl/patches/MinecraftPatch.kt
@@ -1,15 +1,30 @@
 package io.github.wine.warden.patch.impl.patches
 
 import io.github.wine.warden.Warden
+import io.github.wine.warden.event.impl.game.events.StartGameEvent
 import io.github.wine.warden.event.impl.game.events.TickEvent
 import io.github.wine.warden.patch.annotate.Inject
 import io.github.wine.warden.patch.impl.ClassPatch
+import io.github.wine.warden.patch.insertInstructionBeforeReturn
 import org.objectweb.asm.Opcodes.INVOKESTATIC
 import org.objectweb.asm.Type
 import org.objectweb.asm.tree.MethodInsnNode
 import org.objectweb.asm.tree.MethodNode
 
 class MinecraftPatch : ClassPatch("net/minecraft/src/Minecraft") {
+
+    @Inject("startGame")
+    fun patchStartGame(methodNode: MethodNode) {
+        methodNode.insertInstructionBeforeReturn(
+            MethodInsnNode(
+                INVOKESTATIC,
+                Type.getInternalName(this.javaClass),
+                "startGame",
+                "()V",
+                false
+            )
+        )
+    }
 
     @Inject("runTick")
     fun patchRunTick(methodNode: MethodNode) {
@@ -25,6 +40,11 @@ class MinecraftPatch : ClassPatch("net/minecraft/src/Minecraft") {
     }
 
     companion object {
+        @JvmStatic
+        fun startGame() {
+            Warden.bus.publish(StartGameEvent())
+        }
+
         @JvmStatic
         fun runTick() {
             Warden.bus.publish(TickEvent())
